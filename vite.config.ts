4acf912362bf732f48cdb6f2f5d2b2ad0ec32978
Mark <113889912+kno20205@users.noreply.github.com>
2024-02-29 18:07:45 +0100

    remove https from vite server

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,12 @@
-import { defineConfig } from "vite";
-import { dependencies } from "./package.json";
-function renderChunks(deps: Record<string, string>) {
-  let chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
-import { splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
 // https://vitejs.dev/config/
 
 export default defineConfig(({ mode }) => {
   return {
-    server: {
-      https: true,
-    },
-    plugins: [react(), mkcert(), ViteImageOptimizer({})],
+    plugins: [react(), ViteImageOptimizer({})],
     resolve: {
       alias: {
         "@": "/src",
