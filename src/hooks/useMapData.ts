496415026b8e45928b9f19887e3ade5adf80087b
Mark <113889912+kno20205@users.noreply.github.com>
2024-03-27 01:22:53 +0100

    Add categoryName to each object

--- src/hooks/useMapData.ts
+++ src/hooks/useMapData.ts
@@ -11,7 +11,12 @@ function useMapData() {
     try {
       const objectsData = await getObjects();
       const categoriesData = await getCategories();
-
+      // Add categoryName to each object
+      objectsData.forEach((obj) => {
+        obj.categoryName = categoriesData.find(
+          (cat) => cat.id === obj.categoryId
+        )?.name;
+      });
       setObjects(objectsData);
       setCategories(categoriesData);
     } catch (error) {
