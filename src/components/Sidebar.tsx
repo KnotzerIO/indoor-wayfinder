3b0f4bcb5b0c17500cd32a34ff7668b9bcd20b25
Mark <knotzermark@gmail.com>
2025-11-20 22:45:15 +0100

    refactor: svg info icon to react-icons icon

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,6 +9,7 @@ import {
 import { MapDataContext, NavigationContext } from "../pages/Map";
 
 import { navigateToObject } from "@/utils/navigationHelper";
+import { FaInfoCircle } from "react-icons/fa";
 
 interface ParsedObjects {
   [key: string]: {
@@ -61,15 +62,7 @@ function Sidebar() {
           className="font-semibold text-blue-800 text-xs"
         >
           <div className="flex items-center p-2 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50">
-            <svg
-              className="flex-shrink-0 inline w-4 h-4 me-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-            </svg>
+            <FaInfoCircle className="w-4 h-4 mr-2" />
             Check out my new project OpenIndoorMaps
           </div>
         </a>
